b36c22495afbaa965f4afe2dc90d5e68b3e9f580
MaorElfassy <maore@netapp.com>
2024-02-10 21:01:38 +0200

    Adding chat gpt code

--- topics-ingestor/app.py
+++ topics-ingestor/app.py
@@ -1,12 +1,28 @@
+
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+from ingestor import DataIngestor
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
+data_ingestor = DataIngestor()
+
+
+def ingest_tweets():
+    DataIngestor().ingest_tweets()
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(ingest_tweets, 'interval', minutes=1)
+scheduler.start()
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'I am topics ingestor and I will ingest more tweets for all of our customers every 5 minutes!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=1234)
---
+++ topics-ingestor/ingestor.py
@@ -0,0 +1,38 @@
+import logging
+
+from db import mongo_client
+from repo.twitter_gateway_repository import IngestorRepository
+
+
+class DataIngestor:
+    db_client = None
+
+    def __init__(self):
+        self.db_client = mongo_client
+        self.ingestor_repo = IngestorRepository()
+
+    def ingest_tweets(self):
+        logging.info('Ingesting tweets from Twitter...')
+        topics = self.db_client.get_all_topics()
+
+        if topics is None or (len(topics)) == 0:
+            logging.warning('No topics to ingest tweets for')
+            return
+
+        # topics = ['covid', 'vaccine', 'Ronaldo', 'Maor', 'Israel']
+        logging.info('Handling topics: {}'.format(topics))
+
+        for topic in topics:
+            try:
+                logging.info(f'Ingesting tweets topic: {topic}')
+                tweets = self.ingestor_repo.fetch_tweets(topic)
+
+                if (tweets is None) or (len(tweets) == 0):
+                    logging.info(f'No tweets for topic: {topic}')
+                    continue
+                else:
+                    # todo maor - parse tweets somehow
+                    self.db_client.save_tweets(topic, str(tweets))
+                    logging.info(f'Ingested {len(tweets)} tweets for topic: {topic}')
+            except Exception as e:
+                logging.error(f'Error while ingesting tweets for topic: {topic}. Error: {e}')
